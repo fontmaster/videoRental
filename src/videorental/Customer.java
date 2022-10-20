bbca61f65ae27202f4ed389eb1bc07298d6a5a7b
조상정 <sangjeong.jo@samsung.com>
2022-10-21 03:06:23 +0900

    [Refactoring] Add Testcase #8
    - Extract Method
    - statement method 내에 Inline Code, return 값도 Inline시키기

--- src/videorental/Customer.java
+++ src/videorental/Customer.java
@@ -19,39 +19,43 @@ class Customer {
     }
 
     public String statement() {
-        double totalRentalAmount = 0;
-        int frequentRenterPoints = 0;
-        StringBuilder result = new StringBuilder("Rental Record for " + getName() + "\n");
-
-        for (Rental rental : rentals) {
-            double currentRentalFee = rental.getRentalFee();
-            result.append(getRentalResult(rental, currentRentalFee));
-        }
-
-        for (Rental rental : rentals) {
-            frequentRenterPoints += getFrequentRenterPoints(rental);
-        }
-
-        for (Rental rental : rentals) {
-            totalRentalAmount +=  rental.getRentalFee();
-        }
-
-        result.append("Amount owed is ").append(totalRentalAmount).append("\n");
-        result.append("You earned ").append(frequentRenterPoints).append(" frequent renter pointers");
-
-        return result.toString();
+        return "Rental Record for " + getName() + "\n" +
+                getTotalRentalResults() +
+                "Amount owed is " + getTotalRentalFee() + "\n" +
+                "You earned " + getTotalFrequentRenterPoints() + " frequent renter pointers";
     }
 
-    private String getRentalResult(Rental rental, double currentRentalFee) {
-        return "\t" + currentRentalFee + "(" + rental.getMovie().getTitle() + ")" + "\n";
+    private String getTotalRentalResults() {
+        String result = "";
+        for (Rental rental : rentals) {
+            result += "\t" + rental.getRentalFee() + "(" + rental.getMovie().getTitle() + ")" + "\n";
+        }
+        return result;
     }
 
     private int getFrequentRenterPoints(Rental rental) {
-        return isRetnalNewReleaseMovie(rental)? 2 : 1;
+        return isRentalNewReleaseMovie(rental)? 2 : 1;
     }
 
-    private boolean isRetnalNewReleaseMovie(Rental rental) {
+    private boolean isRentalNewReleaseMovie(Rental rental) {
         return (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE) && rental.getDaysRented() > 1;
     }
+
+    private double getTotalRentalFee() {
+        double totalRentalAmount = 0;
+        for (Rental rental : rentals) {
+            totalRentalAmount +=  rental.getRentalFee();
+        }
+        return totalRentalAmount;
+    }
+
+    private int getTotalFrequentRenterPoints() {
+        int frequentRenterPoints = 0;
+        for (Rental rental : rentals) {
+            frequentRenterPoints += getFrequentRenterPoints(rental);
+        }
+        return frequentRenterPoints;
+    }
+
 
 }
