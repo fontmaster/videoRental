4f52e907728060a69a1095786d780784ef8369d8
조상정 <sangjeong.jo@samsung.com>
2022-10-21 02:44:13 +0900

    [Refactoring] Add Testcase #6
    - getRentalFee Method가 Rental class에서 책임을 가지도록 Refactoring

--- src/videorental/Rental.java
+++ src/videorental/Rental.java
@@ -16,4 +16,26 @@ class Rental {
 	public Movie getMovie() {
 		return movie;
 	}
+
+	double getRentalFee() {
+		double rentalFee = 0;
+		switch (getMovie().getPriceCode()) {
+			case Movie.REGULAR:
+				rentalFee = 2;
+				if (getDaysRented() > 2)
+					rentalFee += (getDaysRented() - 2) * 1.5;
+				break;
+
+			case Movie.NEW_RELEASE:
+				rentalFee = getDaysRented() * 3;
+				break;
+
+			case Movie.CHILDRENS:
+				rentalFee = 1.5;
+				if (getDaysRented() > 3)
+					rentalFee += (getDaysRented() - 3) * 1.5;
+				break;
+		}
+		return rentalFee;
+	}
 }
